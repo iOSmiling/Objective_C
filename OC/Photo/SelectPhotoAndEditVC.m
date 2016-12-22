f9d97ffa3ef3feee1fc2e4c708bace13c3fdf12e
iOSmiling <kunlongxue@gmail.com>
2016-12-22 16:51:29 +0800

    UIScrollView

--- OC/Photo/SelectPhotoAndEditVC.m
+++ OC/Photo/SelectPhotoAndEditVC.m
@@ -7,10 +7,12 @@
 //
 
 #import "SelectPhotoAndEditVC.h"
+#import "JFImagePickerController.h"
 
-
-@interface SelectPhotoAndEditVC ()
-
+@interface SelectPhotoAndEditVC ()<JFImagePickerDelegate>
+{
+    NSMutableArray<UIImage *> *_photosArray;
+}
 @end
 
 @implementation SelectPhotoAndEditVC
@@ -20,6 +22,54 @@
 {
     [super viewDidLoad];
     
+     _photosArray = [NSMutableArray new];
+    
+    [self loadNavigationItem];
+    
+}
+
+#pragma mark - UI Build
+-(void)loadNavigationItem
+{
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"多选" style:UIBarButtonItemStylePlain target:self action:@selector(selectMorePhoto)];
+}
+
+#pragma mark - Response Event
+- (void)selectMorePhoto
+{
+    [JFImagePickerController setMaxCount:20];
+    JFImagePickerController *picker = [[JFImagePickerController alloc] initWithRootViewController:[UIViewController new]];
+    picker.pickerDelegate = self;
+    [self.navigationController presentViewController:picker animated:YES completion:nil];
+
+}
+
+#pragma mark - JFImagePicker Delegate -
+
+- (void)imagePickerDidFinished:(JFImagePickerController *)picker{
+    
+    __weak typeof(self) weakself = self;
+    for (ALAsset *asset in picker.assets) {
+        [[JFImageManager sharedManager] imageWithAsset:asset resultHandler:^(CGImageRef imageRef, BOOL longImage) {
+            UIImage *image = [UIImage imageWithCGImage:imageRef];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                
+                [_photosArray addObject:image];
+                
+                
+                
+                
+            });
+        }];
+    }
+    [self imagePickerDidCancel:picker];
+}
+
+- (void)imagePickerDidCancel:(JFImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    [JFImagePickerController clear];
 }
 
 - (void)didReceiveMemoryWarning
--- OC/UISrollView/ScrollViewTwo/ScrollShowTwoVC.m
+++ OC/UISrollView/ScrollViewTwo/ScrollShowTwoVC.m
@@ -7,8 +7,16 @@
 //
 
 #import "ScrollShowTwoVC.h"
+#import "ZXMovieBrowser.h"
 
-@interface ScrollShowTwoVC ()
+#define kScreenWidth  [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height
+
+@interface ScrollShowTwoVC () <ZXMovieBrowserDelegate>
+
+@property (nonatomic, strong, readwrite) ZXMovieBrowser *movieBrowser;
+@property (nonatomic, strong, readwrite) UILabel *titileLabel;
+@property (nonatomic, strong, readwrite) NSMutableArray *movies;
 
 @end
 
@@ -17,8 +25,70 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.navigationController.view.backgroundColor = [UIColor whiteColor];
+    
+    [self setupSubviews];
    
 }
+
+- (void)setupSubviews
+{
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"movies" ofType:@"plist"];
+    NSArray *dictArray = [NSArray arrayWithContentsOfFile:filePath];
+    
+    NSMutableArray *movies = [NSMutableArray array];
+    for (NSDictionary *dict in dictArray)
+    {
+        ZXMovie *movie = [[ZXMovie alloc] init];
+        movie.name = dict[@"name"];
+        movie.coverUrl = dict[@"coverUrl"];
+        [movies addObject:movie];
+    }
+    self.movies = movies;
+    
+    ZXMovieBrowser *movieBrowser = [[ZXMovieBrowser alloc] initWithFrame:CGRectMake(0, 20, kScreenWidth, kMovieBrowserHeight) movies:self.movies currentIndex:1];
+    movieBrowser.delegate = self;
+    [self.view addSubview:movieBrowser];
+    self.movieBrowser = movieBrowser;
+    
+    UILabel *titileLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 330, kScreenWidth, 50)];
+    titileLabel.textAlignment = NSTextAlignmentCenter;
+    titileLabel.textColor = [UIColor blackColor];
+    titileLabel.font = [UIFont systemFontOfSize:15];
+    [self.view addSubview:titileLabel];
+    self.titileLabel = titileLabel;
+}
+
+#pragma mark - ZXMovieBrowserDelegate
+
+- (void)movieBrowser:(ZXMovieBrowser *)movieBrowser didSelectItemAtIndex:(NSInteger)index
+{
+    NSLog(@"跳转详情页---%@", ((ZXMovie *)self.movies[index]).name);
+    
+    UIViewController *movieDetailVc = [[UIViewController alloc] init];
+    movieDetailVc.view.backgroundColor = [UIColor whiteColor];
+    movieDetailVc.title = ((ZXMovie *)self.movies[index]).name;
+    [self.navigationController pushViewController:movieDetailVc animated:YES];
+}
+
+- (void)movieBrowser:(ZXMovieBrowser *)movieBrowser didChangeItemAtIndex:(NSInteger)index
+{
+    NSLog(@"index: %ld", index);
+    
+    self.titileLabel.text = ((ZXMovie *)self.movies[index]).name;
+}
+
+static NSInteger _lastIndex = -1;
+- (void)movieBrowser:(ZXMovieBrowser *)movieBrowser didEndScrollingAtIndex:(NSInteger)index
+{
+    if (_lastIndex != index) {
+        NSLog(@"刷新---%@", ((ZXMovie *)self.movies[index]).name);
+    }
+    _lastIndex = index;
+}
+
 
 - (void)didReceiveMemoryWarning
 {
